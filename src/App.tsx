07282ab3988b8c84ed62a12ac7411fe6f6c1ba8f
anastasiahvozdetska <anastasia9297@gmail.com>
2022-01-20 17:43:38 +0200

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React, { useEffect } from 'react';
-import { useAppSelector } from './hooks/redux';
-import { useDispatch } from 'react-redux';
+import { useAppDispatch, useAppSelector } from './hooks/redux';
 import { userSlice } from './store/reducers/UserSlice';
 import './App.css';
 import { fetchUsers } from './store/reducers/ActionCreators';
 import PostContainer from './components/PostContainer';
 
 function App() {
-  const dispatch = useDispatch();
+  const dispatch = useAppDispatch();
   const { increment } = userSlice.actions
   const { count, users, isLoading, error } = useAppSelector(state => state.userReducer);
 
